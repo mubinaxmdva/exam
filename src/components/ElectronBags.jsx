618cf0e5abe45e72f8136c1c4b8536217f682e6c
mubina <sayfillohh@gmail.com>
2026-07-16 21:38:51 +0500

    ElectronBags-1hour 20min

--- src/components/ElectronBags.jsx
+++ src/components/ElectronBags.jsx
@@ -1,7 +1,20 @@
-
+import {ebag} from "../data/Ebag"
 function ElectronBags() {
   return (
-    <section>ElectronBags</section>
+    <section className="sectionEB">
+      <div className="bagTypes">
+      <a className="a">Water Resistant</a>
+      <a className="a">Charging System</a>
+      <a className="a">Artificial Leather</a>
+      <a className="a">Modern Clothes</a>
+      </div>
+      <div className="imgs">
+
+      {ebag.map((bag) => (
+     <div className="cart"> <img key={bag.id} src={bag.img} alt="" /></div>
+))}
+</div>
+    </section>
   )
 }
 
---
+++ src/data/Ebag.js
@@ -0,0 +1,23 @@
+import ebag1 from "../assets/ebag1.svg"
+import ebag2 from "../assets/ebag2.svg"
+import ebag3 from "../assets/ebag3.svg"
+import ebag4 from "../assets/ebag4.svg"
+
+export const ebag = [
+  {
+    id: 1,
+    img: ebag1,
+  },
+  {
+    id: 2,
+    img: ebag2,
+  },
+  {
+    id: 3,
+    img: ebag3,
+  },
+  {
+    id: 4,
+    img: ebag4,
+  }
+]
