1dde66a3f89f671c98a98a912d2072fa330b9338
mubina <sayfillohh@gmail.com>
2026-07-16 22:10:36 +0500

    Add school bags section

--- src/components/SchoolBags.jsx
+++ src/components/SchoolBags.jsx
@@ -1,7 +1,20 @@
-
+import background2 from "../assets/background2.svg"
+import leftImg from "../assets/leftImg.svg"
+import rightImg from "../assets/rightImg.svg"
+import mainImg from "../assets/mainImg.svg"
 function SchoolBags() {
   return (
-    <section>SchoolBags</section>
+    <section className="sbSection">
+      <img className="img1" src={background2}/>
+      <img className="img2" src={leftImg}/>
+      <img className="img3" src={rightImg}/>
+      <img className="img4" src={mainImg}/>
+      <div className="sbcon">
+          <h3>Creative bag only for you.</h3>
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad voluptates magni.</h2>
+          <p className="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+      </div>
+    </section>
   )
 }
 
