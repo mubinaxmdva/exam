95e8db58d795fb4d1b4b0345070aba60f945ee07
mubina <sayfillohh@gmail.com>
2026-07-16 23:38:23 +0500

    1hour 10min

--- src/components/SchoolBags.jsx
+++ src/components/SchoolBags.jsx
@@ -2,6 +2,7 @@ import background2 from "../assets/background2.svg"
 import leftImg from "../assets/leftImg.svg"
 import rightImg from "../assets/rightImg.svg"
 import mainImg from "../assets/mainImg.svg"
+import Button from "./Button"
 function SchoolBags() {
   return (
     <section className="sbSection">
@@ -11,8 +12,9 @@ function SchoolBags() {
       <img className="img4" src={mainImg}/>
       <div className="sbcon">
           <h3>Creative bag only for you.</h3>
-          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ad voluptates magni.</h2>
-          <p className="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          <h2>Lorem ipsum dolor sit amet consectetur adipiscing elit <br /> sed do eiusmod.</h2>
+          <p className="p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br /> sed do eiusmod tempor incididunt ut labore et dolore <br /> magna aliqua. Ut enim ad minim veniam.</p>
+          <Button>See More</Button>
       </div>
     </section>
   )
