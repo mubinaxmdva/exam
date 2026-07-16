121af04bd41aadabc6362b1543d78adfe6d60255
mubina <sayfillohh@gmail.com>
2026-07-16 16:33:39 +0500

    hero-1hour

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,19 @@
-
+import stylish from "../assets/stylish.svg"
+import Button from "./button"
+import image from "../assets/image.svg"
+import backgroundImage from "../assets/backgroundImage.svg"
 function Hero() {
   return (
-    <section>Hero</section>
+    <section>
+        <div className="herocon">
+        <h3>Look Stylish Be Stylish.</h3>
+        <h1>Look Stylish <br/>Be <img className="stylish" src={stylish} alt="stylish"/> </h1>
+        <p>Before starting this business you should have ideas <br/> about the market and products to Compete with the <br/> Existing Businesses.</p>
+        <Button>Join Shop</Button>
+        </div>
+        <img className="backgroundImage" src={backgroundImage} alt="" />
+        <img className="image" src={image} alt="" />
+    </section>
   )
 }
 
