40b0a50240cdf039f7cce0cb12f8a180b1228baf
mubina <sayfillohh@gmail.com>
2026-07-17 13:15:22 +0500

    logos-10min

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,7 +1,16 @@
+import logo1 from "../assets/logo1.svg"
+import logo2 from "../assets/logo2.svg"
+import logo3 from "../assets/logo3.svg"
+import logo4 from "../assets/logo4.svg"
 
 function Brands() {
   return (
-    <section>Brands</section>
+    <section className="brands">
+      <img src={logo1}/>
+      <img src={logo2}/>
+      <img src={logo3}/>
+      <img src={logo4}/>
+    </section>
   )
 }
 
